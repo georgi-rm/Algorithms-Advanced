9af71f2496f4a923a6312ab93e16f35bae5fe740
georgi-rm <georgi.rm@gmail.com>
2022-02-16 20:06:54 +0200

    Problem 2 "Longest Path" from Lecture: Graphs Bellman-Ford, Longest Path in DAG

--- T02GraphsBellmanFordLongestPathInDAG/P02LongestPath/src/Main.java
+++ T02GraphsBellmanFordLongestPathInDAG/P02LongestPath/src/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
         Map<Integer, List<Edge>> graph = new HashMap<>();
 
-        for (int i = 0; i < numberOfNodes; i++) {
+        for (int i = 1; i <= numberOfNodes; i++) {
             graph.putIfAbsent(i, new ArrayList<>());
         }
         for (int i = 0; i < numberOfEdges; i++) {
